617c4ebc9c5a0ef988466ae55b3ed146d7428ad3
Rodrigo Salas <rsalas@tiempodevelopment.com>
2015-09-17 12:45:18 -0500

    Adding log4j

--- my-app/src/main/java/com/tiempodevelopment/app/App.java
+++ my-app/src/main/java/com/tiempodevelopment/app/App.java
@@ -1,13 +1,19 @@
 package com.tiempodevelopment.app;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	final static Logger log = Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
+    	log.info("Hello World");
     }
 }
