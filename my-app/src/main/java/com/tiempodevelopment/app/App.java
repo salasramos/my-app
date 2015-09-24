2cec71a4a1bc9343bf14ed5ddc3e6e5147719083
salasramos <salas.ramos@gmail.com>
2015-09-24 18:59:38 -0500

    Update App.java

--- my-app/src/main/java/com/tiempodevelopment/app/App.java
+++ my-app/src/main/java/com/tiempodevelopment/app/App.java
@@ -14,6 +14,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    	log.info("Hello World");
+    	log.info("Testing git");
     }
 }
